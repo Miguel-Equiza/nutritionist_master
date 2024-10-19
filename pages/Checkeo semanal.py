57d5f3fa63ae61aaf71f86557f095be0da4470e3
Miguel Equiza <miguelequiza12345@gmail.com>
2024-10-19 12:15:22 +0200

    pagesaaaa

--- pages/Checkeo semanal.py
+++ pages/Checkeo semanal.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 uploaded_file = st.file_uploader("Choose a file", type="xlsx")
 
-if uploaded_file:
-    st.write(f"Filename: {uploaded_file.name}")
-    content = uploaded_file.read()
-    st.write(content)
+if uploaded_file is not None:
+    # Read the Excel file into a DataFrame
+    df = pd.read_excel(uploaded_file)
+
+st.write("Here are your inputs:")
+st.dataframe(df)
