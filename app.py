f9b882973a4667684e66ae6db5f1bbb0d6b1cf58
Miguel Equiza <miguelequiza12345@gmail.com>
2024-10-19 11:32:19 +0200

    sec2 commit

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ st.markdown(
 
     """
 )
+
+uploaded_file = st.file_uploader("Choose a file", type="xlsx")
+
+if uploaded_file:
+    st.write(f"Filename: {uploaded_file.name}")
+    content = uploaded_file.read()
+    st.write(content)
