cc07d69071726e399d190257837f817c1daa8e8b
Miguel Equiza <miguelequiza12345@gmail.com>
2024-10-20 14:48:29 +0200

    put logic atogetheraaaa

--- pages/Primera vez.py
+++ pages/Primera vez.py
@@ -13,13 +13,7 @@ with st.form("fitness_form"):
     age = st.number_input("Age (years)", min_value=1, value=23, step=1, format="%i")
 
     # Add a checkbox to toggle whether the number input is required
-    use_number = st.checkbox("Do you know exactly what your body fat is?", value=False)
-
-    # Show the number input only if the user has checked the box
-    if use_number:
-        boy_fat = st.number_input("Input your body fat", value=0, step=1)  # Default to 0
-    else:
-        body_fat = 0
+    boy_fat = st.number_input("Input your body fat if you know it, if not leave it as 0", value=0, step=1)
 
     # Accepting float input for years of training
     n_years_training = st.number_input("Years of Training", min_value=0.0, value=5.0, step=0.1)
@@ -38,6 +32,13 @@ with st.form("fitness_form"):
 
     # Submit button
     submitted = st.form_submit_button("Submit")
+
+min_calories_meal = 500
+max_calories_meal = 1000
+min_meals = 3
+max_meals = 5
+pre_workout_cals = 300
+post_workout_cals = 200
 
 if submitted:
     # Collect data into a list of tuples (Feature, Value)
@@ -63,11 +64,27 @@ if submitted:
     st.write("Here are your inputs:")
     st.dataframe(df)
 
+    calories, kg_per_week = get_calories(weight, height, age, n_years_training, gender, activity, goal, type_of_weight_change, body_fat)
+    fats, carbs, proteins = macros_cutting(weight, calories, preference_high_fats, preference_high_protein)
+    meals = meal_calories(calories, fats, carbs, proteins, min_calories_meal, max_calories_meal, min_meals, max_meals, pre_workout, post_workout, pre_workout_cals, post_workout_cals)
+    new_data = generate_specific_meals(meals)
+    meals_df = get_meals_df(new_data)
+
+    st.write("Here are your diets:")
+    st.dataframe(meals_df)
+
+
+
+
     # Function to convert DataFrame to Excel and return as BytesIO object
-    def to_excel(df):
+    def to_excel(df, meals_df):
         output = BytesIO()
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
             df.to_excel(writer, index=False, sheet_name='User_Data')
+            unique_n_meals = meals_df['n_meals'].unique()
+            for n in unique_n_meals:
+                df_subset = df[df['n_meals'] == n]
+                df_subset.to_excel(writer, sheet_name=f'n_meals_{n}', index=False)
         processed_data = output.getvalue()
         return processed_data
 
@@ -79,20 +96,3 @@ if submitted:
                        data=xlsx_data,
                        file_name='user_data.xlsx',
                        mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-
-min_calories_meal = 500
-max_calories_meal = 1000
-min_meals = 3
-max_meals = 5
-pre_workout_cals = 300
-post_workout_cals = 200
-
-if submitted:
-    calories, kg_per_week = get_calories(weight, height, age, n_years_training, gender, activity, goal, type_of_weight_change, body_fat)
-    fats, carbs, proteins = macros_cutting(weight, calories, preference_high_fats, preference_high_protein)
-    meals = meal_calories(calories, fats, carbs, proteins, min_calories_meal, max_calories_meal, min_meals, max_meals, pre_workout, post_workout, pre_workout_cals, post_workout_cals)
-    new_data = generate_specific_meals(meals)
-    meals_df = get_meals_df(new_data)
-
-    st.write("Here are your diets:")
-    st.dataframe(meals_df)
